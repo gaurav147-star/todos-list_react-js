7ed84d5f7e3c5548eedafbfdbf4c45d5aaf253d8
gaurav <gauravgupta9158@gmail.com>
2021-08-14 11:11:39 +0530

    Todos List Using react js

---
+++ src/components/TodoList.js
@@ -0,0 +1,73 @@
+import React, { useState,useEffect} from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+
+    let initTodo;
+    if (localStorage.getItem("todos") === null) {
+      initTodo = [];
+    }
+    else {
+      initTodo = JSON.parse(localStorage.getItem("todos"));
+    }
+  
+
+  //const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+
+  return (
+    <>
+      <h1>What's the Plan for Today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+}
+
+export default TodoList;
